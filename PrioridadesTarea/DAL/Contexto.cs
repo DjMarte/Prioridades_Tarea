ae105a0d00b9d48cceec8bdc1b3687c496bbbe9d
DjMarte <alphat098@gmail.com>
2024-01-11 01:00:52 -0400

    Agregue el ConnectionString

---
+++ PrioridadesTarea/DAL/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrioridadesTarea.Models;
+
+namespace PrioridadesTarea.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Prioridades> Prioridades { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { 
+
+        }
+    }
+}
---
+++ PrioridadesTarea/Models/Prioridades.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrioridadesTarea.Models
+{
+    public class Prioridades
+    {
+        [Key]
+        public int PrioridadId { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
+        public string? Descripcion { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
+
+    }
+}
