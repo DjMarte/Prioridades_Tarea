72c54def3f7e892b063257b5b177ad91794abfd4
DjMarte <alphat098@gmail.com>
2024-01-11 13:00:28 -0400

    Recibimiento del contexto en la BLL y creacion de los metodos

---
+++ PrioridadesTarea/BLL/PrioridadesBLL.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrioridadesTarea.DAL;
+using PrioridadesTarea.Models;
+
+namespace PrioridadesTarea.BLL
+{
+    public class PrioridadesBLL
+    {
+        private Contexto _contexto;
+
+        public PrioridadesBLL(Contexto contexto) {
+            _contexto = contexto;
+        }
+
+        public bool Existe(int prioridadId) {
+            return _contexto.Prioridades.Any(o => o.PrioridadId == prioridadId);
+        }
+
+        public bool Insertar(Prioridades prioridad) {
+            _contexto.Prioridades.Add(prioridad); 
+            return _contexto.SaveChanges() > 0;
+        }
+
+        public bool Modificar(Prioridades prioridad) {
+            _contexto.Entry(prioridad).State = EntityState.Modified;
+            return _contexto.SaveChanges() > 0;
+        }
+
+        public bool Guardar(Prioridades prioridad) {
+            if (!Existe(prioridad.PrioridadId))
+                return this.Insertar(prioridad);
+            else
+                return this.Modificar(prioridad);
+        }
+
+        public bool Eliminar(Prioridades prioridad) {
+            _contexto.Entry(prioridad).State = EntityState.Deleted;
+            return _contexto.SaveChanges() > 0;
+        }
+
+        public Prioridades? Buscar(int prioridadId) {
+            return _contexto.Prioridades.Where(o => o.PrioridadId == prioridadId)
+                .AsNoTracking()
+                .SingleOrDefault();
+        }
+    }
+}
